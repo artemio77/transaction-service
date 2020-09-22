65dd7a5bdd29d62aab4ff07b4d85c2347ca58a01
Receptorion898 <dancethebest>
2020-09-22 11:20:42 +0300

    Fix docker compose file

--- docker/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE USER bank_admin WITH PASSWORD 'bank_admin' CREATEDB;
-CREATE DATABASE bank
-    WITH
-    OWNER = bank_admin
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.utf8'
-    LC_CTYPE = 'en_US.utf8'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
